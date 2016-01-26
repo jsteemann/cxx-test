b76238b602b22f8f1aff2c520cb1a3f6ea7ec48b
jsteemann <jan@arangodb.com>
2016-01-26 19:35:13 +0100

    added test output

--- src/Calculator.cpp
+++ src/Calculator.cpp
@@ -1,7 +1,10 @@
 
 #include "calculator/Calculator.h"
 
+#include <iostream>
+
 Value* Calculator::add(Value const& v1, Value const& v2) {
+  std::cout << "test" << std::endl;
   if (v1.isInt() && v2.isInt()) {
     // TODO: check for overflow
     return new ValueInt(v1.toInt() + v2.toInt());
